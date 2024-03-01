e649644c128abf5a7a739c15ff273cd576fbb56c
gachoki30 <140622940+gachoki30@users.noreply.github.com>
2024-03-01 16:32:46 +0300

    Add files via upload
    This is assignment 1

---
+++ Assignment.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+age=input("Enter your age:")
+location=input("Enter your current location:")
+print("Hello",name,"you are",age,"years old.You are currently living in,",location)
+
+
